5325d77f454f79b4bc4f74c1aa0479886af49f77
Karl Williamson <khw@cpan.org>
2020-12-24 18:53:20 -0700

    locale.c: Revise querying a locale
    Now that the LC_NUMERIC toggling is under the same mutex as the rest of
    locale handling, we can simplify things.

--- locale.c
+++ locale.c
@@ -2233,7 +2233,7 @@ Perl_setlocale(const int category, const char * locale)
 #  ifdef USE_LOCALE_NUMERIC
 #    ifdef LC_ALL
 
-        DECLARATION_FOR_LC_NUMERIC_MANIPULATION;
+        bool toggled = FALSE;
 
 #    endif
 
@@ -2246,28 +2246,36 @@ Perl_setlocale(const int category, const char * locale)
             return PL_numeric_name;
         }
 
-#    ifdef LC_ALL
+#  endif
+
+        /* We don't want another thread interrupting and changing the locale
+         * until we have our result safely stashed */
+        SETLOCALE_LOCK;
+
+#  if defined(USE_LOCALE_NUMERIC) && defined(LC_ALL)
 
         /* For an LC_ALL query, switch back to the underlying numeric locale
          * (if we aren't there already) so as to get the correct results.  Our
          * records for all the other categories are valid without switching */
-        if (category == LC_ALL) {
-            STORE_LC_NUMERIC_FORCE_TO_UNDERLYING();
+        if (category == LC_ALL && ! PL_numeric_underlying) {
+            set_numeric_underlying();
+            toggled = TRUE;
         }
 
-#    endif
-#  endif    /* End of has LC_NUMERIC */
+#  endif
 
         retval = save_to_buffer(do_querylocale_r(category),
                                 &PL_setlocale_buf, &PL_setlocale_bufsize, 0);
 
-#  if defined(USE_LOCALE_NUMERIC) && defined(LC_ALL)
+#  ifdef LC_ALL
 
-        if (category == LC_ALL) {
-            RESTORE_LC_NUMERIC();
+        if (toggled) {
+           set_numeric_standard();
         }
 
 #  endif
+
+        SETLOCALE_UNLOCK;
 
         return retval;
     } /* End of querying the current locale */
